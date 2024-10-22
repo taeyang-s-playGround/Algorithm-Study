7d82373a867d1dff7e06c792fe60a61c5964d8ea
tae.y <0211ilyoil@gmail.com>
2024-10-22 10:38:33 +0900

    10950

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
+
+/*
+//10950
+int main() {
+    int T, A, B;
+
+    cin >> T;
+
+    for (int i = 0; i < T; i++) {
+        cin >> A >> B;
+        cout << A + B << "\n";
+    }
+    return 0;
+}
+/*
