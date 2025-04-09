26688c76b9b104ed8815988888d37cdb4e145713
tae.y <0211ilyoil@gmail.com>
2025-04-09 15:05:16 +0900

    ghostbus

---
+++ src/main/java/algo/algorithm/ghostbus.java
@@ -0,0 +1,32 @@
+package algo.algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ghostbus {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int max = -1000001;
+        int min = 1000001;
+
+        for (int i =0; i<count; i++) {
+            int val = Integer.parseInt(st.nextToken());
+            if (val > max) {
+                max = val;
+            }
+            if (val < min) {
+                min = val;
+            }
+        }
+        System.out.println(min + " " + max);
+    }
+
+}
